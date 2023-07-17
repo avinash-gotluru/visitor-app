ee16773b80c2625dc586782ed8670a632db1b7ac
avinashgotluru <avinash.gotluru@gmail.com>
2023-07-17 22:27:01 +0530

    Updated !

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4fdd2cfa461cb19d8a7566875ad63bf0",
-"index.html": "bf2ec04b50711ac31eadc73925bde2e4",
-"/": "bf2ec04b50711ac31eadc73925bde2e4",
-"main.dart.js": "ec6d544e811f2094b7c76457d6fbaa41",
+"index.html": "555528abcdb7d50d769b5fe028c8d589",
+"/": "555528abcdb7d50d769b5fe028c8d589",
+"main.dart.js": "0dc62da79dca351d94a349241fe7a528",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "da634c7ea81db4d07cc57ae2ea8adc59",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
